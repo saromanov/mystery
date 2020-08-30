ed8e00d77fbee7a8a4556270e94e3334875968a4
Sergey <xxsmotur@gmail.com>
2020-08-30 17:03:16 +0500

    Add crypto with aes

--- internal/backend/backend.go
+++ internal/backend/backend.go
@@ -2,8 +2,8 @@ package backend
 
 // Backend defines way for store secrets
 type Backend interface {
-	Get() ([]byte, error)
-	Put([]byte) error
+	Get(masterKey, key []byte) ([]byte, error)
+	Put(masterKey, key, value []byte) error
 }
 
 // MasterPassBackend defines backend for master pass
