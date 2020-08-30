f9260497cd12558e3aae2b3caf38df63dbb81fa8
Sergey <xxsmotur@gmail.com>
2020-08-30 17:08:34 +0500

    Fixing of encrypting and decrypting via aes

---
+++ internal/crypto/aes.go
@@ -0,0 +1,55 @@
+package crypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
+	"io"
+)
+
+// EncryptAES provides encrypting with AES
+func EncryptAES(key []byte, message string) ([]byte, error) {
+	plainText := []byte(message)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	cipherText := make([]byte, aes.BlockSize+len(plainText))
+	iv := cipherText[:aes.BlockSize]
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(cipherText[aes.BlockSize:], plainText)
+
+	return []byte(base64.URLEncoding.EncodeToString(cipherText)), nil
+}
+
+// DecryptAES provides decrypting with AES
+func DecryptAES(key, securemess []byte) ([]byte, error) {
+	cipherText, err := base64.URLEncoding.DecodeString(string(securemess))
+	if err != nil {
+		return nil, err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(cipherText) < aes.BlockSize {
+		return nil, errors.New("ciphertext block size is too short")
+	}
+
+	iv := cipherText[:aes.BlockSize]
+	cipherText = cipherText[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(cipherText, cipherText)
+	return cipherText, nil
+}
