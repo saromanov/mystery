425b1a2b86852b96ce8ef7759e24bdb4f7ef448e
Sergey <xxsmotur@gmail.com>
2020-09-06 10:42:52 +0500

    implementation of Get method

--- internal/backend/postgres/postgres.go
+++ internal/backend/postgres/postgres.go
@@ -41,6 +41,12 @@ func New(c *config.Config) (backend.Backend, error) {
 
 // Get defines getting a secret from backend
 func (m *Postgres) Get(masterKey, key []byte) (backend.Secret, error) {
+	var r Mystery
+	if err := m.db.Find(&r, &Mystery{
+		Key: string(key),
+	}); err != nil {
+		return backend.Secret{}, fmt.Errorf("unable to get secret: %v", err)
+	}
 	return backend.Secret{}, nil
 }
 
