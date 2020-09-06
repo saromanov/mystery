a9a7bf568f3467e6afd3be51b6eacdf2fe9d2f36
Sergey <xxsmotur@gmail.com>
2020-09-06 09:42:09 +0500

    implementation of the store method

--- internal/mystery/put.go
+++ internal/mystery/put.go
@@ -3,12 +3,15 @@ package mystery
 import (
 	"errors"
 	"fmt"
+
+	"github.com/saromanov/mystery/internal/backend"
 )
 
 var (
 	errNoMasterPass = errors.New("master pass is not defined")
 	errNoKey        = errors.New("key is not defined")
 	errNoValue      = errors.New("value is not defined")
+	errNoBackend    = errors.New("backend is not defined")
 )
 
 // PutRequest provides struct for sending to Put
@@ -16,6 +19,7 @@ type PutRequest struct {
 	MasterPass string
 	Key        string
 	Value      string
+	Backend    backend.Backend
 }
 
 // validate provides validation of data
@@ -29,6 +33,9 @@ func (p PutRequest) validate() error {
 	if p.Value == "" {
 		return errNoValue
 	}
+	if p.Backend == nil {
+		return errNoBackend
+	}
 	return nil
 }
 
@@ -36,6 +43,12 @@ func (p PutRequest) validate() error {
 func Put(p PutRequest) error {
 	if err := p.validate(); err != nil {
 		return fmt.Errorf("put: unable to validate data: %v", err)
+	}
+	if err := p.Backend.Put([]byte(p.MasterPass), backend.Secret{
+		Key:   []byte(p.Key),
+		Value: []byte(p.Value),
+	}); err != nil {
+		return fmt.Errorf("put: unable to store data: %v", err)
 	}
 	return nil
 }
